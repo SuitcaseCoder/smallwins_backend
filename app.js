7cdec2d63595c6e63b8d0a96f157e9d0f78d6cc5
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2023-01-25 09:39:13 -0600

    login consoles

--- app.js
+++ app.js
@@ -209,7 +209,6 @@ app.post('/register', (req, res)=> {
       // let sql = `INSERT INTO users (first_name, last_name, username, email, hash) VALUES (${firstname}, ${lastname}, ${username}, ${email}, ${hash})`;
       // db.query(sql, [firstname, lastname, username, email, hash],
       // [firstname, lastname, username, email, hash],
-      // 📌📌📌📌📌📌 pick up right here
       db.query(`INSERT INTO users (first_name, last_name, username, email, hash) VALUES ('${firstname}', '${lastname}', '${username}', '${email}', '${hash}');`,[firstname, lastname, username, email, hash], 
       (err, result, fields)=> {
         
